faa1fd3389d911ea510135bc688dd287ebfc1e53
IgorSwat <igorswat2002@o2.pl>
2026-05-18 17:58:41 +0200

    Add missing Hindi EOS

--- src/phonemis/base/tagger/constants.h
+++ src/phonemis/base/tagger/constants.h
@@ -4,6 +4,17 @@
 
 namespace phonemis::tagger::constants {
 
-inline const std::unordered_set<char32_t> kEosCharacters = {U'.', U'?', U'!', U';'};
+inline const std::unordered_set<char32_t> kEosCharacters = {
+    U'.',
+    U'?',
+    U'!',
+    U';',
+    U'…', // Ellipsis
+    U'|', // ASCII Pipe (often used as Hindi Purna Viram)
+    U'।', // Hindi Purna Viram (U+0964)
+    U'॥', // Hindi Deergh Viram (U+0965)
+    U'¿', // Spanish Inverted Question Mark (U+00BF)
+    U'¡', // Spanish Inverted Exclamation Mark (U+00A1)
+};
 
 } // namespace phonemis::tagger::constants
